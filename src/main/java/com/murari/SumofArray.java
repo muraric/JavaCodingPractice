1f3209bd02f1ffd4fdae5d6376a7a6283063ef4a
murari <muraric@gmail.com>
2024-05-19 16:28:04 -0400

    New scenarios

--- src/main/java/com/murari/SumofArray.java
+++ src/main/java/com/murari/SumofArray.java
@@ -1,11 +1,9 @@
 package com.murari;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class SumofArray {
     public static int getSumofArrayElements(Integer[] intArray) {
-        return Arrays.asList(intArray).stream().reduce(0, Integer::sum);
+        return Arrays.stream(intArray).reduce(0, Integer::sum);
     }
 }
